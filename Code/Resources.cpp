487bb30561a394d1475ddbaf7a8c2525efd2948d
Amadeus <amadeusfidos@gmail.com>
2024-10-29 12:53:50 +0200

    Resource update

---
+++ Code/Resources.cpp
@@ -0,0 +1,81 @@
+#include "Resources.h"
+
+void Resources::setConcrete(int newAmount)
+{
+    concrete = newAmount;
+}
+
+int Resources::getConcrete()
+{
+    return concrete;
+}
+
+void Resources::setSteel(int newAmount)
+{
+    steel = newAmount;
+}
+
+int Resources::getSteel()
+{
+    return steel;
+}
+
+void Resources::setEnergy(int newAmount)
+{
+    energy = newAmount;
+}
+
+int Resources::getEnergy()
+{
+    return energy;
+}
+
+void Resources::setWater(int newAmount)
+{
+    water = newAmount;
+}
+
+int Resources::getWater()
+{
+    return water;
+}
+
+void Resources::setBudget(float newAmount)
+{
+    budget = newAmount;
+}
+
+float Resources::getBudget()
+{
+    return budget;
+}
+
+void Resources::setPopulation(int newAmount)
+{
+    population = newAmount;
+}
+
+int Resources::getPopulation()
+{
+    return population;
+}
+
+void Resources::setMorale(int newAmount)
+{
+    morale = newAmount;
+}
+
+int Resources::getMorale()
+{
+    return morale;
+}
+
+void Resources::setWood(int newAmount)
+{
+    wood = newAmount;
+}
+
+int Resources::getWood()
+{
+    return wood;
+}
