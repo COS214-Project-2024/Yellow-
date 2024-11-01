7c2733620f5b2df7b5777ed9606b4cbf0dbdc96f
Amadeus <amadeusfidos@gmail.com>
2024-11-01 16:25:05 +0200

    Cost calculation with building dimensions

--- Code/BuildingFactory.cpp
+++ Code/BuildingFactory.cpp
@@ -8,13 +8,13 @@ bool BuildingFactory::useBuildingResources(int numSteel, int numConcrete, int nu
         int c = city->stuff.res->getConcrete();
         int w = city->stuff.res->getWood();
         float b = city->stuff.res->getBudget();
-        if (s < numSteel || c < numConcrete || w < numWood) 
+        if (s < (numSteel * cellCount)  || c < (numConcrete * cellCount) || w < (numWood * cellCount)) 
             return false;
         else {
-            city->stuff.res->setConcrete(c - numConcrete);
-            city->stuff.res->setSteel(s - numSteel);
-            city->stuff.res->setWood(w - numWood);
-            city->stuff.res->setBudget(b - numBudget);
+            city->stuff.res->setConcrete(c - (numConcrete * cellCount));
+            city->stuff.res->setSteel(s - (numSteel * cellCount));
+            city->stuff.res->setWood(w - (numWood * cellCount));
+            city->stuff.res->setBudget(b - (numBudget * cellCount));
         }
     } else {
         return false;
