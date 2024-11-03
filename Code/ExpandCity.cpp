37d33ccf9c4ecd623429611d0f6c9ef3be408199
Saskia Steyn <u17267162@tuks.co.za>
2024-11-03 15:34:29 +0200

    Fixed expand city

--- Code/ExpandCity.cpp
+++ Code/ExpandCity.cpp
@@ -5,6 +5,10 @@ Policies* ExpandCity::implementPolicy(string stateType, string prevStateColour, 
         cout << "\033[38;5;48mApparantly we have a city of rabbits, we are expanding it to make space for all the new babies \033[0m" << endl;
         return new Policies("ExpandCity");
     }
+    if(stateType == "People" && currStateColour == "Red" && prevStateColour == "Red"){
+        cout << "\033[38;5;48mApparantly we have a city of rabbits, we are expanding it to make space for all the new babies \033[0m" << endl;
+        return new Policies("ExpandCity");
+    }
 
     return nullptr;
 
